0c3205cca7457d110ef3af69fdf0c91c2ab04218
Steven Wittens <steven@acko.net>
2012-08-23 01:29:48 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+VENDOR="
+vendor/underscore.js
+"
+
+SRC="
+src/Common.js
+src/Block.js
+src/Factory.js
+src/Program.js
+src/Snippet.js
+src/Graph/Graph.js
+src/Graph/Node.js
+src/Graph/Outlet.js
+"
+
+cat $VENDOR $SRC > build/ShaderGraph.js
+cat $SRC > build/ShaderGraph-core.js
+
+curl --data-urlencode "js_code@build/ShaderGraph.js" 	\
+	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS" \
+	http://closure-compiler.appspot.com/compile	\
+	> build/ShaderGraph.min.js
+
+curl --data-urlencode "js_code@build/ShaderGraph-core.js" 	\
+	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS" \
+	http://closure-compiler.appspot.com/compile	\
+	> build/ShaderGraph-core.min.js
