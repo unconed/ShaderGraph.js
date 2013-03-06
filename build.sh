0e42acf6066fd7ceeeb8fb66dd9e242414c26ad5
Steven Wittens <steven@acko.net>
2013-03-06 00:22:44 -0800

    build script

--- build.sh
+++ build.sh
@@ -19,12 +19,12 @@ cat $SRC > build/ShaderGraph-core.js
 
 if [ -z "$SKIP_MINIFY" ]; then
 curl --data-urlencode "js_code@build/ShaderGraph.js" 	\
-	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS" \
+	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS&language=ECMASCRIPT5" \
 	http://closure-compiler.appspot.com/compile	\
 	> build/ShaderGraph.min.js
 
 curl --data-urlencode "js_code@build/ShaderGraph-core.js" 	\
-	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS" \
+	-d "output_format=text&output_info=compiled_code&compilation_level=SIMPLE_OPTIMIZATIONS&language=ECMASCRIPT5" \
 	http://closure-compiler.appspot.com/compile	\
 	> build/ShaderGraph-core.min.js
 fi
